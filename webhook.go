636a6dcc3180d7eac6ae95a8c809acffca1eb0d2
tomoyaueno29 <u.t012807@gmail.com>
2020-06-20 17:19:02 +0900

    Update webhook.go

--- webhook.go
+++ webhook.go
@@ -36,7 +36,6 @@ func init() {
 	}
 }
 
-
 func Webhook(w http.ResponseWriter, r *http.Request) {
 	client, err := linebot.New(secrets.LineChannelSecret, secrets.LineChannelAccessToken)
 	if err != nil {
@@ -66,7 +65,7 @@ func Webhook(w http.ResponseWriter, r *http.Request) {
 
 func lineSecretsKmsKeyName() string {
 	prjID := os.Getenv("GCP_PROJECT_ID")
-	keyRingName :=os.Getenv("KMS_KEY_RING_NAME")
+	keyRingName := os.Getenv("KMS_KEY_RING_NAME")
 	keyName := os.Getenv("KMS_LINE_SECRETS_KEY_NAME")
 	return fmt.Sprintf("projects/%s/locations/global/keyRings/%s/cryptoKeys/%s", prjID, keyRingName, keyName)
 }
